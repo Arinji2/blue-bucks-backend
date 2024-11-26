e0f376f9b4fd980c990457447a30a6eb32efee2e
Arinji2 <arinjaydhar205@gmail.com>
2024-11-26 16:29:18 +0530

    Finish Up Basic API handling

--- api/go.mod
+++ api/go.mod
@@ -2,4 +2,10 @@ module github.com/blue-bucks/backend
 
 go 1.23.2
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/go-chi/render v1.0.3
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/ajg/form v1.5.1 // indirect
--- api/main.go
+++ api/main.go
@@ -2,11 +2,54 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load()
-	fmt.Println("Look Ma! No errors :D")
+	r := chi.NewRouter()
+
+	setupEnvironment()
+	r.Use(SkipLoggingMiddleware)
+	r.Get("/health", healthCheckHandler)
+
+	http.Handle("/", r)
+	fmt.Println("Starting Blue-Bucks Backend on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setupEnvironment() {
+	err := godotenv.Load()
+	if err != nil {
+		isProduction := os.Getenv("ENVIRONMENT") == "PRODUCTION"
+		if !isProduction {
+			log.Fatal("Error loading .env file")
+		} else {
+			fmt.Println("Using Production Environment")
+		}
+	}
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Blue-Bucks Backend: Health Check"))
+	render.Status(r, http.StatusOK)
+}
+
+func SkipLoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/health" {
+			next.ServeHTTP(w, r)
+			return
+		}
+		middleware.Logger(next).ServeHTTP(w, r)
+	})
 }
